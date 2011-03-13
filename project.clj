4438928f80fa529460623736df498f8757a0e500
Ray Miller <ray@1729.org.uk>
2011-03-13 13:06:54 +0000

    Initial check-in

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ray1729/bio.ensembl "1.0.0-SNAPSHOT"
+  :description "Clojure functions for interacting with EnsEMBL database"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clojureql "1.0.0"]
+                 [mysql/mysql-connector-java "5.1.15"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
---
+++ test/ray1729/bio/ensembl/test/core.clj
@@ -0,0 +1,6 @@
+(ns ray1729.bio.ensembl.test.core
+  (:use [ray1729.bio.ensembl.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
